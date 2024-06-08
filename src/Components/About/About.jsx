b7532bf50e4c26e1836cf2538c9e70c61bddb05a
Khang Huynh <khanghuynh8078@gmail.com>
2024-06-08 00:31:28 -0700

    create about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -18,8 +18,7 @@ const About = () => {
                 <div className="about-para">
                     <p>I am an experienced Frontend Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>                </div>
                     <p>My passion for frontend development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
-            </div>
-            <div className="about-skills">
+                    <div className="about-skills">
                 <div className="about-skill">
                     <p>HTML & CSS</p><hr style={{width:"50%"}}/>
                 </div>
@@ -32,6 +31,24 @@ const About = () => {
                 <div className="about-skill">
                     <p>Next JS</p><hr style={{width:"50%"}}/>
                 </div>
+            </div>
+            </div>
+            
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>90+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>15+</h1>
+                <p>HAPPY CLIENTS</p>
             </div>
         </div>
     </div>
