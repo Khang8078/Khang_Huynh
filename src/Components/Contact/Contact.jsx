3121336c22018134a6e9e38446c34639153d8b5a
Khang Huynh <khanghuynh8078@gmail.com>
2024-06-13 23:57:57 -0700

    create contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+        <div className="contact-title">
+            <h1>Get in touch</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's talk</h1>
+                <p>I'm currently avaliable to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
+                <div className="contact-details">
+                    <div className="contact-detail">
+                        <img src={mail_icon} alt="" /><p>greatstackdev@gmail.com</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={call_icon} alt="" /><p>+111-111-111</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={location_icon} alt="" /><p>CA, United States</p>
+                    </div>
+                </div>
+            </div>
+            <form action="" className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text" placeholder='Enter your name' name='name' />
+                <label htmlFor="">Your Email</label>
+                <input type="text" />
+            </form>
+        </div>
+    </div>
   )
 }
 
